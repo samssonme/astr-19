a38ead7bafc17d4b0b3b460ca51a60f1f1de3222
samssonme <146788664+samssonme@users.noreply.github.com>
2023-10-11 15:39:16 -0700

    Add files via upload

---
+++ session2.py
@@ -0,0 +1,18 @@
+#sum of two floats!
+a=3.5+1.5
+print(a)
+print(type(a))
+#difference of two integer numbers!
+
+a=4
+b=2
+c=a-b
+print(c)
+print(type(c))
+#the product of a floating point number and an integer
+
+a=4
+b=2.5
+c=a*b
+print(c)
+print(type(c))
